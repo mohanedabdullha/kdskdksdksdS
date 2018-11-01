1515292d786bd38ed2786249f62ecdc0c6f2e4b9
mohanedabdullha <43791989+mohanedabdullha@users.noreply.github.com>
2018-11-01 21:07:47 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`Magic Server`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`مهما تعدّدت النّساء حبيبتي فالأصل أنت `,'https://www.twitch.tv/MeeRcY')
 
 });
 client.login(process.env.BOT_TOKEN);
